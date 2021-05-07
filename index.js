8451beb652c006ea2668a37e1cb9624ee7e12910
EduardoMay <eduardo_may@outlook.com>
2021-05-06 22:59:02 -0500

    feat: proceso para generar y pegar archivo y verficar status del internet

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
-const fa = require("fs")
+const fs = require("fs")
+const { exec } = require("child_process")
+const dns = require('dns');
 
-const existFile = () => {
-  return fa.existsSync("/var/www/html/example/index.js")
+const contentFile = (ssid, password) => {
+  return `
+  ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+  update_config=1
+  country=MX
+
+  network={
+      ssid="${ssid}"
+      psk="${password}"
+      key_mgmt=WPA-PSK
+  }
+  `
 }
 
-console.log(existFile());
+const generateFile = (ssid, password) => {
+  const existsFile = fs.existsSync("/var/www/html/example/index.js")
+
+  if (existsFile) {
+    fs.writeFile('./archivo1.txt', 'línea 1\nLínea 2', error => {
+      if (error)
+        console.log(error);
+      else
+        console.log('El archivo fue creado');
+    });
+  } else {
+    // Crear archivo wpa_supplicant
+    fs.writeFile(`${process.env.PROGRAMFILES}\\..\\Users\\SuperAdmin\\Documents\\destino.txt`, contentFile(ssid, password), error => {
+      if (error)
+        console.log(error);
+      else {
+        console.log('El archivo fue creado');
+        // pegar el archivo en la ruta especifica
+        // fs.copyFileSync('./archivo1.txt', `${process.env.PROGRAMFILES}\\..\\Users\\SuperAdmin\\Documents\\destino.txt`);
+
+        // Correr comando en bash para reiniciar servicio de autohotsapd
+        exec("git push origin master", (error, stdout, stderr) => {
+          if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+          }
+          if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            dns.lookup('example.org', (err, address, family) => {
+              // Si hay internet
+              if (!err) {
+
+              }
+            });
+            return;
+          }
+          console.log(`stdout: ${stdout}`);
+        });
+      }
+    });
+  }
+}
+
+console.log(generateFile());
